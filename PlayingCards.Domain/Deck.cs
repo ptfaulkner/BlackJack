1c2d436ea752d243b326631cc66f9c0db0caf979
Patrick Faulkner <ptfaulkner@gmail.com>
2014-12-22 15:10:35 -0500

    Change deck to use stack instead of list

--- PlayingCards.Domain/Deck.cs
+++ PlayingCards.Domain/Deck.cs
@@ -11,29 +11,28 @@ namespace PlayingCards.Domain
             Reset();
         }
  
-        public List<Card> Cards { get; set; }
+        public Stack<Card> Cards { get; set; }
  
         public void Reset()
         {
-            Cards = Enumerable.Range(1, 4)
+            Cards = new Stack<Card>(
+                Enumerable.Range(1, 4)
                 .SelectMany(s => Enumerable.Range(2, 13)
                     .Select(c => new Card
                     {
-                        Suit = (Suit)s,
-                        Number = (CardNumber)c
-                    }))
-                .ToList();
+                        Suit = (Suit) s,
+                        Number = (CardNumber) c
+                    })));
         }
  
         public void Shuffle()
         {
-            Cards = Cards.OrderBy(c => Guid.NewGuid()).ToList();
+            Cards = new Stack<Card>(Cards.OrderBy(c => Guid.NewGuid()));
         }
  
         public Card TakeCard()
         {
-            Card card = Cards.FirstOrDefault();
-            Cards.Remove(card);
+            Card card = Cards.Pop();
  
             return card;
         }
