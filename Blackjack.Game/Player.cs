c0249efa182bbf45dfd928697badb27016e3d07b
Patrick Faulkner <ptfaulkner@gmail.com>
2014-12-22 11:00:34 -0500

    Add player score method

--- Blackjack.Game/Player.cs
+++ Blackjack.Game/Player.cs
@@ -12,13 +12,69 @@ namespace Blackjack.Game
         public string Name { get; set; }
         public List<Card> Hand { get; set; }
 
-        public int Score()
+        public short Score()
         {
-            int score = 0;
+            short score = 0;
 
-            Hand.Sum(c => c.Number);
+            foreach (Card nonAceCard in Hand.Where(c => c.Number != CardNumber.Ace))
+            {
+                score += CardValue(nonAceCard);
+            }
+
+            foreach (Card ace in Hand.Where(c => c.Number == CardNumber.Ace))
+            {
+                if ((score + 11) < 21)
+                {
+                    score += 11;
+                    continue;
+                }
+                score += 1;
+            }
 
             return score;
+        }
+
+        private short CardValue(Card card)
+        {
+            short value;
+            switch (card.Number)
+            {
+                case CardNumber.Two:
+                    value = 2;
+                    break;
+                case CardNumber.Three:
+                    value = 3;
+                    break;
+                case CardNumber.Four:
+                    value = 4;
+                    break;
+                case CardNumber.Five:
+                    value = 5;
+                    break;
+                case CardNumber.Six:
+                    value = 6;
+                    break;
+                case CardNumber.Seven:
+                    value = 7;
+                    break;
+                case CardNumber.Eight:
+                    value = 8;
+                    break;
+                case CardNumber.Nine:
+                    value = 9;
+                    break;
+                case CardNumber.Ten:
+                case CardNumber.Jack:
+                case CardNumber.Queen:
+                case CardNumber.King:
+                    value = 10;
+                    break;
+                default:
+                    value = 0;
+                    break;
+            }
+
+            return value;
         }
     }
 }
---
+++ PlayingCards.Tests/BlackjackPlayerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blackjack.Game;
+using PlayingCards.Domain;
+using Xunit;
+
+namespace PlayingCards.Tests
+{
+    public class BlackjackPlayerTests
+    {
+        [Fact]
+        public void CanCalculatePlayerScore()
+        {
+            Player player = new Player();
+            player.Hand = new List<Card>
+            {
+                new Card {Number = CardNumber.Three},
+                new Card {Number = CardNumber.Queen}
+            };
+
+            Assert.Equal(13, player.Score());
+        }
+    }
+}
