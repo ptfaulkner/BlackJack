83170ea0ac3e1d3984d03088e45a986963586711
Patrick Faulkner <ptfaulkner@gmail.com>
2014-12-22 11:04:03 -0500

    Add some more score unit tests

--- PlayingCards.Tests/BlackjackPlayerTests.cs
+++ PlayingCards.Tests/BlackjackPlayerTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Blackjack.Game;
 using PlayingCards.Domain;
 using Xunit;
@@ -12,16 +8,67 @@ namespace PlayingCards.Tests
     public class BlackjackPlayerTests
     {
         [Fact]
-        public void CanCalculatePlayerScore()
+        public void CanCalculatePlayerScoreWithoutAce()
         {
-            Player player = new Player();
-            player.Hand = new List<Card>
+            Player player = new Player
             {
-                new Card {Number = CardNumber.Three},
-                new Card {Number = CardNumber.Queen}
+                Hand = new List<Card>
+                {
+                    new Card {Number = CardNumber.Three},
+                    new Card {Number = CardNumber.Queen}
+                }
             };
 
             Assert.Equal(13, player.Score());
+        }
+
+        [Fact]
+        public void CanCalculatePlayerScoreWithOneAceOver21()
+        {
+            Player player = new Player
+            {
+                Hand = new List<Card>
+                {
+                    new Card {Number = CardNumber.Three},
+                    new Card {Number = CardNumber.Queen},
+                    new Card {Number = CardNumber.Ace}
+                }
+            };
+
+            Assert.Equal(14, player.Score());
+        }
+
+        [Fact]
+        public void CanCalculatePlayerScoreWithOneAceUnder21()
+        {
+            Player player = new Player
+            {
+                Hand = new List<Card>
+                {
+                    new Card {Number = CardNumber.Three},
+                    new Card {Number = CardNumber.Two},
+                    new Card {Number = CardNumber.Ace}
+                }
+            };
+
+            Assert.Equal(16, player.Score());
+        }
+
+        [Fact]
+        public void CanCalculatePlayerScoreWithTwoAcesUnder21()
+        {
+            Player player = new Player
+            {
+                Hand = new List<Card>
+                {
+                    new Card {Number = CardNumber.Three},
+                    new Card {Number = CardNumber.Two},
+                    new Card {Number = CardNumber.Ace},
+                    new Card {Number = CardNumber.Ace}
+                }
+            };
+
+            Assert.Equal(17, player.Score());
         }
     }
 }
