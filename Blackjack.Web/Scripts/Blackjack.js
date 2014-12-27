30a420b655e9b0e0fe948065c5db02cbdb0c17c8
Patrick Faulkner <ptfaulkner@gmail.com>
2014-12-26 22:03:54 -0500

    Ditched react for knockout for better js debugging

---
+++ Blackjack.Web/Scripts/Blackjack.js
@@ -0,0 +1,37 @@
+﻿var Blackjack = (function () {
+  var self = this,
+    websocket;
+
+
+  var blackjack = function (game) {
+    self.game = game || {};
+    self.connectionStatus = ko.observable("Not Connected");
+
+    self.connect();
+  };
+
+  self.connect = function () {
+    var uri = 'ws://localhost:51364/api/blackjack';
+    websocket = new WebSocket(uri);
+
+    websocket.onopen = function () {
+      self.connectionStatus("Connected");
+    };
+
+    websocket.onerror = function (event) {
+      self.connectionStatus("Connection Error :(");
+    }
+
+    websocket.onmessage = function (event) {
+
+    }
+  }
+
+  self.send = function () {
+    websocket.send("some data");
+  }
+
+  return blackjack;
+})();
+
+ko.applyBindings(new Blackjack());
