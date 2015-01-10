7cb6d13d9c8f0f4e6a1ee80b059156a0b9bdb408
Patrick Faulkner <ptfaulkner@gmail.com>
2015-01-10 14:47:17 -0500

    Add player dto object

---
+++ Blackjack.Web/Models/CurrentPlayerDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Blackjack.Game;
+
+namespace Blackjack.Web.Models
+{
+    public class CurrentPlayerDto
+    {
+        public Player Player { get; set; }
+        public Player Dealer { get; set; }
+        public IList<Player> TablePlayers { get; set; }
+        public HandStatus GameStatus { get; set; }
+    }
+}
