cba7b52e3a4fd45e186773fd8a7ebaded6c637ed
Patrick Faulkner <ptfaulkner@gmail.com>
2019-08-13 21:39:28 -0400

    update currentPlayer to be a stateless component

--- Blackjack.Web/Scripts/currentPlayer.jsx
+++ Blackjack.Web/Scripts/currentPlayer.jsx
@@ -2,43 +2,34 @@
 import { CSSTransitionGroup } from 'react-transition-group';
 import Card from './card';
 
-export default class CurrentPlayer extends React.Component {
-  constructor(props) {
-    super(props);
+const CurrentPlayer = (props) => {
 
-    this.hit = this.hit.bind(this);
-    this.stay = this.stay.bind(this);
-    this.deal = this.deal.bind(this);
-    this.chooseButtons = this.chooseButtons.bind(this);
-  }
+  const hit = () => {
+    props.doGameAction('Hit');
+  };
 
-  hit() {
-    this.props.doGameAction('Hit');
-  }
+  const stay = () => {
+    props.doGameAction('Stay');
+  };
 
-  stay() {
-    this.props.doGameAction('Stay');
-  }
+  const deal = () => {
+    props.doGameAction('Deal');
+  };
 
-  deal() {
-    this.props.doGameAction('Deal');
-  }
-
-  chooseButtons() {
-    const props = this.props || {};
+  const chooseButtons = () => {
     const player = props.player || {};
 
     if (player.handStatus === 'Open' && player.isTurnToHit) {
       return (
         <div className="turn-buttons">
-          <input type="button" value="Hit" className='form-item button' onClick={this.hit} />
-          <input type="button" value="Stay" className='form-item button' onClick={this.stay} />
+          <input type="button" value="Hit" className='form-item button' onClick={hit}/>
+          <input type="button" value="Stay" className='form-item button' onClick={stay}/>
         </div>
       );
     } else if (props.gameStatus !== 'Open') {
       return (
         <div className="turn-buttons">
-          <input type="button" value="Deal" className='form-item button' onClick={this.deal} />
+          <input type="button" value="Deal" className='form-item button' onClick={deal}/>
         </div>
       );
     } else if (player.handStatus === 'Open') {
@@ -54,38 +45,37 @@ export default class CurrentPlayer extends React.Component {
         </div>
       );
     }
-  }
+  };
 
-  render() {
-    const player = this.props.player || {};
-    const hand = player.hand || [];
-    const buttons = this.chooseButtons();
-    const cards = hand.map((card, index) => {
-        const key = `${card.suit}-${card.number}`;
-        return <Card key={key} suit={card.suit} number={card.number} index={index} />;
-      });
+  const player = props.player || {};
+  const hand = player.hand || [];
+  const buttons = chooseButtons();
+  const cards = hand.map((card, index) => {
+    const key = `${card.suit}-${card.number}`;
+    return <Card key={key} suit={card.suit} number={card.number} index={index}/>;
+  });
 
-    return (
-      <div className='text-center'>
-        <div className='player'>
-          <span>{player.name}</span>
-          <div>
-            Winning Status: <span>{player.winningStatus}</span><br />
-            Hand Status: <span>{player.handStatus}</span>
-          </div>
-          <div className='hand-container'>
-            <CSSTransitionGroup
-                transitionName="animate"
-                transitionEnterTimeout={500}
-                transitionLeaveTimeout={300}
-            >
-              {cards}
-            </CSSTransitionGroup>
-          </div>
-          <br className='clear-fix' />
-          {buttons}
+  return (
+    <div className='text-center'>
+      <div className='player'>
+        <span>{player.name}</span>
+        <div>
+          Winning Status: <span>{player.winningStatus}</span><br/>
+          Hand Status: <span>{player.handStatus}</span>
         </div>
+        <div className='hand-container'>
+          <CSSTransitionGroup
+            transitionName="animate"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={300}>
+            {cards}
+          </CSSTransitionGroup>
+        </div>
+        <br className='clear-fix'/>
+        {buttons}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default CurrentPlayer;
