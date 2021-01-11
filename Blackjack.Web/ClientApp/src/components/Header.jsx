efb00e6876e806181c029b00bcba7671a6dc928e
Patrick Faulkner <ptfaulkner@gmail.com>
2021-01-10 19:37:18 -0500

    update header component

--- Blackjack.Web/ClientApp/src/components/Header.jsx
+++ Blackjack.Web/ClientApp/src/components/Header.jsx
@@ -1,43 +1,33 @@
-﻿import React from 'react';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+﻿import React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-const Header = (props) => {
-  const newPlayersArray = props.newPlayers || [];
-  const newPlayers = newPlayersArray.join(', ');
+const Header = ({ newPlayers, connectionStatus }) => {
+  const newPlayersArray = newPlayers || [];
+  const newPlayersString = newPlayersArray.join(", ");
   let newPlayersSpan = (
-    <CSSTransition      
-      classNames="fade"
-      timeout={500}
-      key='no-new-players'
-    >
+    <CSSTransition classNames="fade" timeout={500} key="no-new-players">
       <span />
     </CSSTransition>
-   );
+  );
 
   if (newPlayers.length) {
     newPlayersSpan = (
-      <CSSTransition      
-        classNames="fade"
-        timeout={500}
-        key='new-players'
-      >
-        <span>Joining now: {newPlayers}</span>
+      <CSSTransition classNames="fade" timeout={500} key="new-players">
+        <span>Joining now: {newPlayersString}</span>
       </CSSTransition>
     );
   }
 
   return (
     <header>
-      <div className='title'>
+      <div className="title">
         <span>Blackjack &spades;&diams;&hearts;&clubs;</span>
       </div>
-      <div className='new-players'>
-        <TransitionGroup>
-          {newPlayersSpan}
-        </TransitionGroup>
+      <div className="new-players">
+        <TransitionGroup>{newPlayersSpan}</TransitionGroup>
       </div>
-      <div className='connection-status'>
-        <span>{props.connectionStatus}</span>
+      <div className="connection-status">
+        <span>{connectionStatus}</span>
       </div>
     </header>
   );
